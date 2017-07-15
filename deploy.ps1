91ef39d14a7de19fef7160296bedf6b2745a895f
Patrick El-Azem <p@elazem.com>
2017-07-14 21:23:25 -0400

    Add params for SQL image

--- deploy.ps1
+++ deploy.ps1
@@ -102,7 +102,7 @@ if(Test-Path $parametersFilePath) {
 # Start the deployment
 Write-Host "Starting deployment...";
 if(Test-Path $parametersFilePath) {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath
 } else {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath
 }
